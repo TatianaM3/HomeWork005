5525f7a483266f1306eab9bac3bd0e2e415b38d3
Татьяна <yuki-shuichi@mail.ru>
2022-09-03 13:33:50 +0300

    Второй вариант решения

---
+++ Task#38(2)/Program.cs
@@ -0,0 +1,41 @@
+﻿void FillArray(double[] collection){
+    int lendth = 5;
+    int i = 0;
+    while (i < lendth){
+        collection[i] = new Random().Next(1, 100);
+        i++;
+    }
+}
+void PrintArray(double[] col){
+    int count = col.Length;
+    int pos = 0;
+    while(pos < count){
+        Console.Write(col[pos] + ", ");
+        pos++;
+    }
+}
+double[] array = new double[5];
+
+FillArray(array);
+PrintArray(array);
+
+double max = array[0];
+int i = 0;
+
+while (i < array.Length){
+    if(max < array[i]){
+        max = array[i];
+    }
+    i++;
+}
+
+double min = array[0];
+
+while (i < array.Length){
+    if (min > array[i]){
+        min = array[i];
+    }
+    i++;
+}
+double dif = max - min;
+Console.WriteLine("Разница между максимальным и минимальным значением массива = " + dif);
